d48b3b1b01e5e0c68999a6db42c0f6844c6dce0d
sani <sanit_ak@cmu.ac.th>
2021-12-28 13:48:38 +0700

    เสร็จ

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,37 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double a[],int N,double b[]){
+    for (int i=0; i < N ;i++){
+        b[0]+=a[i];
+    }
+    b[0]/=N;
+
+    double sd;
+    for(int j = 0; j < N; ++j) {
+    sd += pow(a[j] - b[0], 2);
+    }
+    b[1] = sqrt(sd/ N);  
+
+    double gm=1;
+    for (int o = 0; o < N; o++){
+        gm = gm * a[o];
+        b[2] = pow(gm, (double)1 / N);
+    }
+
+    double hg;
+    for(int k=0;k < N ; ++k){
+        hg += (1/a[k]);
+    }
+    b[3] = N/hg;
+
+    for(int u = 0; u < N; ++u) {
+        if (a[u]>b[4]){
+            b[4] =a[u];
+        }
+        if (a[u]<b[4]){
+            b[5] =a[u];
+        }
+    }
+}
